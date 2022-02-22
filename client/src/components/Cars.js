c1d65a29907b73d82d85b6e207752b4f6fb54f9d
snrtn <snrtn94@gmail.com>
2022-02-22 17:31:03 +0100

    add

---
+++ client/src/components/Cars.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Cars = () => {
+  return (
+    <div className="cars">
+      <p>Marque: </p>
+      <p>Coileur: </p>
+    </div>
+  );
+};
+
+export default Cars;
---
+++ client/src/components/Mycars.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Car from "./Cars";
+
+class Mycars extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <Car />
+      </div>
+    );
+  }
+}
+
+export default Mycars;
